0eb3ce150592ed3f8df87149f47c63f7aa254fd0
Cristian Guzman <cristians089@gmail.com>
2023-11-19 09:46:45 -0500

    Se agregan pruebas test para las apps

--- backend/apps/places/tests.py
+++ backend/apps/places/tests.py
@@ -1,3 +1,47 @@
 from django.test import TestCase
+from .models import Place
+from .serializers import PlaceSerializer
+from .choices import PlaceTypeChoices
+from datetime import datetime
 
-# Create your tests here.
+class PlaceModelTestCase(TestCase):
+    def setUp(self):
+        self.place = Place.objects.create(
+            t_name='Sala de Reuniones 1',
+            t_description='Descripción de la sala de reuniones',
+            n_type=PlaceTypeChoices.MEETING_ROOM,
+            t_observation='Observaciones sobre la sala',
+            n_price=100.0,
+        )
+
+    def test_place_str_representation(self):
+        self.assertEqual(
+            str(self.place),
+            f"Place {self.place.pk_id} - Type: {self.place.get_n_type_display()}"
+        )
+
+    def test_place_model_fields(self):
+        self.assertEqual(self.place.t_name, 'Sala de Reuniones 1')
+        self.assertEqual(self.place.t_description, 'Descripción de la sala de reuniones')
+        self.assertEqual(self.place.n_type, PlaceTypeChoices.MEETING_ROOM)
+        self.assertEqual(self.place.t_observation, 'Observaciones sobre la sala')
+        self.assertEqual(self.place.n_price, 100.0)
+
+class PlaceSerializerTestCase(TestCase):
+    def setUp(self):
+        self.place_data = {
+            't_name': 'Sala de Reuniones 2',
+            't_description': 'Descripción de la sala de reuniones 2',
+            'n_type': PlaceTypeChoices.MEETING_ROOM,
+            't_observation': 'Observaciones sobre la sala 2',
+            'n_price': 150.0,
+        }
+        self.place = Place.objects.create(**self.place_data)
+
+    def test_place_serializer(self):
+        serializer = PlaceSerializer(instance=self.place)
+        self.assertEqual(serializer.data['t_name'], 'Sala de Reuniones 2')
+        self.assertEqual(serializer.data['t_description'], 'Descripción de la sala de reuniones 2')
+        self.assertEqual(serializer.data['n_type'], PlaceTypeChoices.MEETING_ROOM)
+        self.assertEqual(serializer.data['t_observation'], 'Observaciones sobre la sala 2')
+        self.assertEqual(serializer.data['n_price'], '150.00')
--- backend/apps/reservation/tests.py
+++ backend/apps/reservation/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from django.utils import timezone
+from ..authuser.models import Employee, Student, Customer
+from ..payments.models import Payment
+from .models import Reservation
+from rest_framework import status
 
-# Create your tests here.
+
+class ReservationModelTest(TestCase):
+    def setUp(self):
+        # Crear instancias necesarias para las pruebas
+        customer = Customer.objects.create(name='Cliente de prueba', email='casasas@gm.com')
+        student = Student.objects.create(name='Estudiante de prueba', email='casaqwes@gm.com')
+        employee = Employee.objects.create(name='Empleado de prueba', email='crfdasas@gm.com')
+        payment = Payment.objects.create(pk_id=1)
+
+        self.reservation = Reservation.objects.create(
+            d_reservation=timezone.now(),
+            b_status=True,
+            fk_customer=customer,
+            fk_student=student,
+            fk_employee=employee,
+            fk_payment=payment
+        )
+
+    def test_str_method(self):
+        self.assertEqual(str(self.reservation), f"Reservation {self.reservation.pk_id}")
+
+    def test_verbose_names(self):
+        self.assertEqual(Reservation._meta.verbose_name, 'Reservación')
+        self.assertEqual(Reservation._meta.verbose_name_plural, 'Reservaciones')
+
+    # Agrega más pruebas según sea necesario
